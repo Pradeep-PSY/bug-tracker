c004aee24b2da9e26fd4ebf40a2ddd99125777c1
Danny12244 <pradeep.py725@gmail.com>
2023-04-03 22:59:22 +0530

    frontend

---
+++ bug-tracker/src/reducer/reducer.js
@@ -0,0 +1,11 @@
+
+const intialState = {
+    isAuth : false,
+}
+
+export const appReducer = (state = intialState, {type, payload}) => {
+    switch(type){
+        default : return state
+    }
+}
+
---
+++ bug-tracker/src/store/store.js
@@ -0,0 +1,17 @@
+import {
+  legacy_createStore,
+  combineReducers,
+  compose,
+  applyMiddleware,
+} from 'redux';
+import thunk from 'redux-thunk';
+import { appReducer } from '../reducer/reducer';
+
+const rootReducer = combineReducers({ auth: appReducer });
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = legacy_createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
