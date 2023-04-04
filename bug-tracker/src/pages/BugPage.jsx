74313b5e130b7becf9b5e7f95e453ae099166ee9
Danny12244 <pradeep.py725@gmail.com>
2023-04-05 00:00:18 +0530

    drag and drop basic implementation done

--- bug-tracker/src/pages/BugPage.jsx
+++ bug-tracker/src/pages/BugPage.jsx
@@ -1,5 +1,6 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import {
     Box,
     Modal,
@@ -18,6 +19,13 @@ import {
 
 const BugPage = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        setItems(report)
+    }, [])
+
     const report = [
         {
             id: 'item-1',
@@ -41,46 +49,80 @@ const BugPage = () => {
         },
 
     ]
+
+    const reorder = (arr, startIndex, endIndex) => {
+        const result = Array.from(arr)
+        const [removed] = result.splice(startIndex, 1)
+        result.splice(endIndex, 0, removed)
+        console.log(result)
+        return result
+    }
+
+    const handleDragEnd = (result) => {
+        console.log(result)
+        
+        const reorderedItems = reorder(items, result.source.index, result.destination.index)
+        console.log(reorderedItems)
+        setItems(reorderedItems)
+    }
     return (
-        <Box border={'1px solid'}>
-            <Button onClick={onOpen}>Open Modal</Button>
-            {
-                report.map((el) => (
-                    <Box key={el.id} bg='tomato' w='10%' m={2} p={4} color='white'>
-                        {el.name}
-                    </Box>
-                ))
-            }
+        <DragDropContext onDragEnd={handleDragEnd}>
+
+            <Box border={'1px solid'}>
+                <Droppable droppableId='droppable'>
+                    {
+                        (provided, snapshot) => (
+                            <Box {...provided.droppableProps} ref={provided.innerRef} bg={snapshot.isDraggingOver ? 'lightgreen' : ""}>
+                                {/* <Button onClick={onOpen}>Open Modal</Button> */}
+                                {
+                                    items.map((el, i) => (
+                                        <Draggable key={el.id} draggableId={el.id} index={i}>
+                                            {(provided, snapshot) => (
+                                                <Box ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} bg={snapshot.isDragging ? "tomato" : "grey"} w='10%' m={2} p={4} color='white'>
+                                                    {el.name}
+                                                </Box>
+                                            )}
+
+                                        </Draggable>
+                                    ))
+                                }
+                                {provided.placeholder}
+                            </Box>
+                        )
+                    }
 
 
+                </Droppable>
 
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Create your account</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={6}>
-                        <FormControl>
-                            <FormLabel>First name</FormLabel>
-                            <Input placeholder='First name' />
-                        </FormControl>
 
-                        <FormControl mt={4}>
-                            <FormLabel>Last name</FormLabel>
-                            <Input placeholder='Last name' />
-                        </FormControl>
-                    </ModalBody>
+                <Modal isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Create your account</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody pb={6}>
+                            <FormControl>
+                                <FormLabel>First name</FormLabel>
+                                <Input placeholder='First name' />
+                            </FormControl>
 
-                    <ModalFooter>
-                        <Button colorScheme='blue' mr={3}>
-                            Save
-                        </Button>
-                        <Button onClick={onClose}>Cancel</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </Box>
+                            <FormControl mt={4}>
+                                <FormLabel>Last name</FormLabel>
+                                <Input placeholder='Last name' />
+                            </FormControl>
+                        </ModalBody>
 
+                        <ModalFooter>
+                            <Button colorScheme='blue' mr={3}>
+                                Save
+                            </Button>
+                            <Button onClick={onClose}>Cancel</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+            </Box>
+
+        </DragDropContext>
     )
 }
 
